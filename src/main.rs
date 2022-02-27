db4be86dfe6740ce04846f30a2ba2c3bc41ddd27
rellyrell24 <jg.techie24@gmail.com>
2022-02-27 16:26:34 -0600

    complete tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpServer};
+use actix_service::Service;
 mod state;
 mod to_do;
 mod views;
@@ -8,7 +9,19 @@ mod processes;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let app = App::new().configure(views::views_factory);
+        let app = App::new().wrap_fn(|req, srv| {
+            if *&req.path().contains("/item") {
+                match views::tokens::process_token(&req) {
+                    Ok(_token) => println!("the token is passable"),
+                    Err(message) => println!("token error: {}", message)
+                }
+            }
+            let fut = srv.call(req);
+            async {
+                let result = fut.await?;
+                Ok(result)
+            }
+        }).configure(views::views_factory);
         return app
     })
         .bind("127.0.0.1:8000")?
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -2,6 +2,7 @@ use actix_web::web;
 mod path;
 mod auth;
 mod to_do;
+pub mod tokens;
 
 pub fn views_factory(app: &mut web::ServiceConfig) {
     auth::auth_factory(app);
