9cd84fd20e4143dcf6b7379c2d21664253e6f527
rellyrell24 <jg.techie24@gmail.com>
2022-02-27 18:31:18 -0600

    err: edit/delete button not working

---
+++ javascript/main.js
@@ -0,0 +1,66 @@
+let createButton = document.getElementById("create-button");
+createButton.addEventListener("click", createItem);
+
+function createItem() {
+    let title = document.getElementById("name");
+    let call = apiCall("/item/create/" + title.value, "POST");
+    call.send();
+    title.value = null;
+}
+
+function renderItems(items, processType, elementId, processFunction) {
+    let placeholder = "<div>";
+    let itemsMeta = [];
+    for (i = 0; i < items.length; i++) {
+        let title = items[i]["title"];
+        let placeholderId = processType + "-" + title.replaceAll(" ", "-");
+        placeholder += "<div>" + title + "<button " + 'id="' + placeholderId + '">' + processType + '</button>' + "</div>";
+        itemsMeta.push({"id": placeholderId, "title": title});
+    }
+    placeholder += "</div>";
+    document.getElementById(elementId).innerHTML = placeholder;
+    for (i = 0; i < itemsMeta.length; i++) {
+        document.getElementById(itemsMeta[i]["id"]).addEventListener("click", processFunction);
+    }
+}
+
+function apiCall(url, method) {
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', function() {
+        if (this.readyState === this.DONE) {
+            renderItems(JSON.parse(this.responseText)["pending_items"], "edit", "pendingItems", editItem);
+            renderItems(JSON.parse(this.responseText)["done_items"], "delete", "doneItems", deleteItem);
+        }
+    });
+    xhr.open(method, url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('user-token', "token");
+    return xhr
+}
+
+function editItem() {
+    let title = this.id.replaceAll("-", " ").replace("edit", "");
+    let call = apiCall("/item/edit", "PUT");
+    let json = {
+        "title": title,
+        "status": "done"
+    };
+    call.send(JSON.stringify(json));
+}
+
+function deleteItem() {
+    let title = this.id.replaceAll("-", " ").replace("delete", "");
+    let call = apiCall("/item/delete", "POST");
+    let json = {
+        "title": title,
+        "status": "done"
+    };
+    call.send(JSON.stringify(json));
+}
+
+function getItems() {
+    let call =  apiCall("/item/get", "GET");
+    call.send()
+}
+getItems();
